245298626b6a6f6ce4670ba64e2c43930a5dec07
Miltonnare <miltonnareblessed@gmail.com>
2025-03-08 21:06:16 +0300

    Python Learning

---
+++ Calc.py
@@ -0,0 +1,21 @@
+a=4
+b=7
+
+def Add ():
+    result=a+b
+    print(result)
+
+def Sub ():
+    result=a-b
+    print(result)
+
+def Mult():
+    result=a*b
+    print(result)
+def Div ():
+    result=a/b
+    print(result)
+Add()
+Sub()
+Mult()
+Div()
